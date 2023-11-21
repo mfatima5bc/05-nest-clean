deca69e797771e924c9225f9a8c2551c9cac8948
Maria de Fátima <mfatima5bc@gmail.com>
2023-11-21 09:00:59 -0300

    feat: create auth decorator

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,11 +5,11 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { env } from 'src/env';
 import { z } from 'zod';
 
-const tokenSchema = z.object({
+const tokenPayloadSchema = z.object({
   sub: z.string().uuid(),
 });
 
-export type TokenSchema = z.infer<typeof tokenSchema>;
+export type UserPayload = z.infer<typeof tokenPayloadSchema>;
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -23,7 +23,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: TokenSchema) {
-    return tokenSchema.parse(payload);
+  async validate(payload: UserPayload) {
+    console.log('hi');
+    return tokenPayloadSchema.parse(payload);
   }
 }
