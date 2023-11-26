f705e50f1cf82ecde305eb568ad7e7b97ee9a930
Maria de Fátima <mfatima5bc@gmail.com>
2023-11-25 22:32:09 -0300

    wip: configure e2e setup and create tests to create use and authenticate

---
+++ src/controllers/authenticate.controller.e2e-spec.ts
@@ -0,0 +1,42 @@
+import { AppModule } from "@/app.module";
+import { Test } from '@nestjs/testing';
+import { INestApplication } from "@nestjs/common";
+import request from 'supertest';
+import { PrismaService } from "@/prisma/prisma.service";
+import { hash } from "bcryptjs";
+
+describe('Authentication (E2E)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    prisma = moduleRef.get(PrismaService);
+
+    await app.init();
+  });
+
+  test('[POST] /sessions', async () => {
+    await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'Johndoe@email.com',
+        password: await hash('123456', 8),
+      }
+    })
+
+    const response = await request(app.getHttpServer()).post('/sessions').send({
+      email: 'Johndoe@email.com',
+      password: '123456',
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({
+      access_token: expect.any(String),
+    });
+  });
+});
--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -1,18 +1,28 @@
 import swc from 'unplugin-swc';
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   test: {
     include: ['**/*.e2e-spec.ts'],
     globals: true,
+    alias: {
+      '@src': './src',
+      '@test': './test',
+    },
     root: './',
+    setupFiles: ['./test/setup-e2e.ts'],
+    exclude: [...configDefaults.exclude, '**/data/pg/**'],
+  },
+  resolve: {
+    alias: {
+      '@src': './src',
+      '@test': './test',
+    },
   },
   plugins: [
     tsConfigPaths(),
-    // This is required to build the test files with SWC
     swc.vite({
-      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
       module: { type: 'es6' },
     }),
   ],
