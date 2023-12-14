de14b26f5b4c1e03ce00592748fe6ece73ff7a4e
Maria de Fátima <mfatima5bc@gmail.com>
2023-12-13 21:02:28 -0300

    wip: start controller to save attachments

---
+++ src/infra/http/controllers/upload-attachment.controller.e2e-spec.ts
@@ -0,0 +1,38 @@
+import { AppModule } from '@/infra/app.module';
+import { Test } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { JwtService } from '@nestjs/jwt';
+import { StudentFactory } from 'test/factories/make-student';
+import { DatabaseModule } from '@/infra/database/database.module';
+
+describe('Upload attachment (E2E)', () => {
+  let app: INestApplication;
+  let studentFactory: StudentFactory;
+  let jwt: JwtService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    studentFactory = moduleRef.get(StudentFactory);
+    jwt = moduleRef.get(JwtService);
+
+    await app.init();
+  });
+
+  test('[POST] /attachments', async () => {
+    const user = await studentFactory.makePrismaStudent();
+    const accessToken = jwt.sign({ sub: user.id.toString() });
+
+    const response = await request(app.getHttpServer())
+      .post('/attachments')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .attach('file', './test/e2e/sample-upload.jpeg');
+
+    expect(response.statusCode).toBe(201);
+  });
+});
