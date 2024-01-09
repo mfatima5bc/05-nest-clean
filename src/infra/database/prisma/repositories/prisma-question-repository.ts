a16f5339b18fc6e3319f18105af236e4b8b59109
Maria de Fátima <mfatima5bc@gmail.com>
2024-01-09 18:13:35 -0300

    wip: create cache repository

--- src/infra/database/prisma/repositories/prisma-question-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-repository.ts
@@ -8,11 +8,13 @@ import { QuestionAttachmentsRepository } from '@/domain/forum/application/reposi
 import { QuestionDetails } from '@/domain/forum/enterprise/entities/value-objects/question-details';
 import { PrismaQuestionDetailsMapper } from '../mappers/prisma-question-details-mapper';
 import { DomainEvents } from '@/core/events/domain-events';
+import { CacheRepository } from '@/infra/cache/cache-repository';
 
 @Injectable()
 export class PrismaQuestionRepository implements QuestionRepository {
   constructor(
     private prisma: PrismaService,
+    private cache: CacheRepository,
     private questionAttachmentsRepository: QuestionAttachmentsRepository,
   ) {}
 
@@ -39,6 +41,13 @@ export class PrismaQuestionRepository implements QuestionRepository {
   }
 
   async findDetailsBySlug(slug: string): Promise<QuestionDetails | null> {
+    const cacheHit = await this.cache.get(`question:${slug}:details`);
+
+    if (cacheHit) {
+      const cacheData = JSON.parse(cacheHit);
+      return cacheData;
+    }
+
     const question = await this.prisma.question.findFirst({
       where: {
         slug,
@@ -50,7 +59,14 @@ export class PrismaQuestionRepository implements QuestionRepository {
     });
 
     if (!question) return null;
-    return PrismaQuestionDetailsMapper.toDomain(question);
+
+    const questionDetails = PrismaQuestionDetailsMapper.toDomain(question);
+
+    await this.cache.set(
+      `question:${slug}:details`,
+      JSON.stringify(questionDetails),
+    );
+    return questionDetails;
   }
 
   async findManyRecent({ page }: PaginationParams): Promise<Question[]> {
@@ -85,7 +101,7 @@ export class PrismaQuestionRepository implements QuestionRepository {
     await Promise.all([
       this.prisma.question.update({
         where: {
-          id: question.id.toString(),
+          id: data.id,
         },
         data,
       }),
@@ -95,6 +111,7 @@ export class PrismaQuestionRepository implements QuestionRepository {
       this.questionAttachmentsRepository.deleteMany(
         question.attachments.getRemovedItems(),
       ),
+      this.cache.delete(`question:${data.slug}:details`),
     ]);
 
     DomainEvents.dispatchEventsForAggregate(question.id);
