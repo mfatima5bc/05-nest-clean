8a14e41050798c71fbd2a059bc26fd1befe82dca
Maria de Fátima <mfatima5bc@gmail.com>
2024-01-10 10:58:43 -0300

    wip: cache repository

--- src/infra/env/env.service.ts
+++ src/infra/env/env.service.ts
@@ -7,6 +7,6 @@ export class EnvService {
   constructor(private configService: ConfigService<env, true>) {}
 
   get<T extends keyof env>(key: T) {
-    return this.configService.get<T>(key, { infer: true });
+    return this.configService.get(key, { infer: true });
   }
 }
