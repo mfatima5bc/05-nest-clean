3bf646d93b94f5e7d42cdb3e61f348b435cbb634
Maria de Fátima <mfatima5bc@gmail.com>
2024-01-24 15:35:23 -0300

    fix: prisma engine for deploy and install swagger

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { EnvService } from './env/env.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Forum backend api')
+    .setDescription('API for scholl forum')
+    .setVersion('1.0')
+    .addTag('forum')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const envService = app.get(EnvService);
   const port = envService.get('PORT');
